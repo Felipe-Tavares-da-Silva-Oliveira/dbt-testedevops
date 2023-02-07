9fecfd6e1c3384c0f0ec0fdc1e5bd9a8360f8c95
bruno.campos <bruno.campos@bv.com.br>
2023-02-07 14:54:58 -0300

    Tira o comando dbt seed e aponta apenas para target dev para teste

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-dbt seed --target dev --profiles-dir .
-dbt seed --target prod --profiles-dir .
+# dbt seed --target dev --profiles-dir .
+# dbt seed --target prod --profiles-dir .
 dbt deps --profiles-dir .  # Pulls the most recent version of the dependencies listed in your packages.yml from git
 dbt debug --target dev --profiles-dir .
-dbt debug --target prod --profiles-dir .
-dbt run --target prod --profiles-dir .
-dbt test --target prod --profiles-dir .
+# dbt debug --target prod --profiles-dir .
+# dbt run --target prod --profiles-dir .
+# dbt test --target prod --profiles-dir .
