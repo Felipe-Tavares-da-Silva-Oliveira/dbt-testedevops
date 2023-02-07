ecfaf90b26988a0fbe8b227798bc8443477f777d
bruno.campos <bruno.campos@bv.com.br>
2023-02-07 14:45:48 -0300

    Adiciona o comando dbt seed no arquivo script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+dbt seed --target dev --profiles-dir .
+dbt seed --target prod --profiles-dir .
 dbt deps --profiles-dir .  # Pulls the most recent version of the dependencies listed in your packages.yml from git
 dbt debug --target dev --profiles-dir .
 dbt debug --target prod --profiles-dir .
