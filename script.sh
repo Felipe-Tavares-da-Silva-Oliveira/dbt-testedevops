c098ae50625e015d5ec458327341490a86813f19
Felipe Tavares da Silva Oliveira <54562192+Felipe-Tavares-da-Silva-Oliveira@users.noreply.github.com>
2023-02-09 15:50:24 -0300

    update compilador shell scripts.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 dbt seed --target dev --profiles-dir .
 dbt seed --target prod --profiles-dir .
 dbt deps --profiles-dir .  # Pulls the most recent version of the dependencies listed in your packages.yml from git
@@ -12,8 +12,6 @@ echo 'start collect path'
 IFS=/
 current_path1=$(pwd)
 current_path2=${current_path1[-1]}
-
-echo $current_path2 'path para salvar a docs.json'
 
 ls ./target
 ls /secrets/dbt-service-keyfile
