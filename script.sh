ba9934b71e2b6f27139a50716afe117ce60bf277
Felipe Tavares da Silva Oliveira <54562192+Felipe-Tavares-da-Silva-Oliveira@users.noreply.github.com>
2023-02-09 16:07:26 -0300

    update scripts.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 dbt seed --target dev --profiles-dir .
 dbt seed --target prod --profiles-dir .
 dbt deps --profiles-dir .  # Pulls the most recent version of the dependencies listed in your packages.yml from git
@@ -8,12 +8,9 @@ dbt run --target prod --profiles-dir .
 dbt test --target prod --profiles-dir .
 dbt docs generate --no-compile --target dev --profiles-dir .
 
-echo 'start collect path'
-IFS=/
-current_path1=$(pwd)
-current_path2=${current_path1[-1]}
-
+current_path=`basename "$PWD"`
+echo "current path é $current_path"
 ls ./target
 ls /secrets/dbt-service-keyfile
 gcloud auth activate-service-account --key-file=/secrets/dbt-service-keyfile
-gsutil cp -r ./target/catalog.json gs://dbt_testeintegration/$current_path2
+gsutil cp -r ./target/catalog.json gs://dbt_testeintegration/$current_path
