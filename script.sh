11c27096f532aae6c91a1f2c2d1f109c5d218389
Felipe Tavares da Silva Oliveira <54562192+Felipe-Tavares-da-Silva-Oliveira@users.noreply.github.com>
2023-02-09 15:18:51 -0300

    include trace on logs scripts.sh

--- script.sh
+++ script.sh
@@ -8,9 +8,12 @@ dbt run --target prod --profiles-dir .
 dbt test --target prod --profiles-dir .
 dbt docs generate --no-compile --target dev --profiles-dir .
 
+echo 'start collect path'
 IFS=/
 current_path1=$(pwd)
 current_path2=${current_path1[-1]}
+
+echo $current_path2 'path para salvar a docs.json'
 
 ls ./target
 ls /secrets/dbt-service-keyfile
