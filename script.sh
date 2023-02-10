227cbbfcef77345e37eca37d48d3e260c142af2b
bruno.campos <bruno.campos@bv.com.br>
2023-02-10 11:27:43 -0300

    Ajusta comando tr do script.sh

--- script.sh
+++ script.sh
@@ -8,7 +8,7 @@ dbt run --target prod --profiles-dir .
 dbt test --target prod --profiles-dir .
 dbt docs generate --no-compile --target dev --profiles-dir .
 
-current_path=`cat dbt_project.yml | grep -i name | awk -F: '{print $2}' | tr -dc '[:alnum:]\n\r\_' | awk '{sub(" ","")}1'`
+current_path=`cat dbt_project.yml | grep -i name | awk -F: '{print $2}' | tr -dc '[:alnum:]\_' | awk '{sub(" ","")}1'`
 echo "current path é $current_path"
 ls ./target
 pathbucket="gs://dbt_testeintegration/$current_path/"
