d020a73c9559f3d5e290f8dbd681835f67a87b94
bruno.campos <bruno.campos@bv.com.br>
2023-03-08 18:10:58 -0300

    Ajusta invoke.go para ter a funcao de health check

--- invoke.go
+++ invoke.go
@@ -7,7 +7,7 @@ import (
         "os/exec"
 )
 func handler(w http.ResponseWriter, r *http.Request) {
-        log.Print("helloworld: received a request")
+        log.Print("dbt: received a request")
 cmd := exec.Command("/bin/sh", "script.sh")
         cmd.Stdout = os.Stdout
  cmd.Stderr = os.Stderr
@@ -16,13 +16,20 @@ cmd := exec.Command("/bin/sh", "script.sh")
   log.Fatalf("cmd.Run() failed with %s\n", err)
  }
 }
+
+func healthHandler(w http.ResponseWriter, r *http.Request){
+        log.Print("dbt: health check OK")
+        w.WriteHeader(http.StatusOK)
+}
+
 func main() {
-        log.Print("helloworld: starting server...")
+        log.Print("dbt: starting server...")
 http.HandleFunc("/", handler)
+http.HandleFunc("/health", healthHandler)
 port := os.Getenv("PORT")
         if port == "" {
                 port = "8080"
         }
-log.Printf("helloworld: listening on %s", port)
+log.Printf("dbt: listening on %s", port)
         log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
