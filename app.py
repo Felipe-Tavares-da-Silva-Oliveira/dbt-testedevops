49b05cbf92b403e6e0ebbe467f4f26967060d39c
bruno.campos <bruno.campos@bv.com.br>
2023-03-08 21:43:17 -0300

    Ajusta aplicacao flask

--- app.py
+++ app.py
@@ -4,16 +4,18 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/dbt-run')
-def dbt_run():
-    print("dbt: received a request")
-    subprocess.run(["/bin/sh", "script.sh"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
-    return 'OK'
 
-@app.route('/health')
+@app.route('/')
 def health_check():
     print("dbt: health check ok")
     return jsonify({"status":"ok"})
+
+
+@app.route('/dbt-run')
+def dbt_run():
+    print("dbt: received a request")
+    subprocess.run(["/bin/sh", "/dbt/script.sh"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+    return jsonify({"execucaoDbt":"ok"})
 
 if __name__ == 'main':
     port = int(os.environ.get('PORT', 8080))
