e2906f96429d250bdae6e9b3a1262d4362acf79d
bruno.campos <bruno.campos@bv.com.br>
2023-03-09 10:03:43 -0300

    Ajusta metodo dbt-run

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify
-import subprocess
+from subprocess import check_output
 import os
 
 app = Flask(__name__)
@@ -9,11 +9,13 @@ def health_check():
     print("dbt: health check ok")
     return jsonify({"status":"ok"})
 
-# @app.route('/dbt-run')
-# def dbt_run():
-#     print("dbt: received a request")
-#     subprocess.run(["/bin/sh", "/dbt/script.sh"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
-#     return jsonify({"execucaoDbt":"ok"})
+@app.route('/dbt-run')
+def dbt_run():
+    print("dbt: received a request")
+    #subprocess.run(["/bin/sh", "/dbt/script.sh"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+    #return jsonify({"execucaoDbt":"ok"})
+    stdout = check_output(['/bin/sh', '/dbt/script.sh']).decode('utf-8')
+    return stdout
 
 if __name__ == 'main':
     port = int(os.environ.get('PORT', 8080))
